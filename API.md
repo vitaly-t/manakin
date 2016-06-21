75fa7c79d63118b8f527a87055824e3961b2e248
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2016-06-21 11:23:46 +0100

    adding support for method `log`.

--- API.md
+++ API.md
@@ -2,48 +2,55 @@
 
 There are three ways in which the library can be used:
 
-* [global] - overrides the global `console.warn` and `console.error`, to be used directly  
-* [local] - creates a new object for local usage, without affecting anything globally 
+* [global] - overrides methods `log`, `warn` and `error` of the `console` object  
+* [local] - creates a new object for local usage, without any global changes 
 * [shared] - uses the same, module-wide object
+
+Note that unlike methods `warn` and `error`, if you do not set bright color for method `log`,
+it will redirect to the default implementation.
 
 ### global
 
-Using colors globally for `console.warn` and `console.error`:
+Overriding methods `log`, `warn` and `error` of the `console` object:
 
 ```js
-require('manakin').global; // overrides the console methods
+require('manakin').global;
 
-console.warn(val1, val2, ...);  // yellow output for the entire process
-console.error(val1, val2, ...); // red output for the entire process
+console.log(val1, val2, ...);   // redirects to the default implementation 
+console.warn(val1, val2, ...);  // uses yellow color
+console.error(val1, val2, ...); // uses red color
 ```
 
-Setting bright colors globally:
+Setting bright colors:
 
 ```js
 var con = require('manakin').global;
 
-con.warn.bright = true; // use bright yellow for all warnings in the process;
-con.error.bright = true; // use bright red for all errors in the process;
+con.log.bright = true; // use bright white for `console.log`;
+con.warn.bright = true; // use bright yellow for `console.warn`;
+con.error.bright = true; // use bright red for `console.error`;
 ```
 
 ### local
 
-Using color output with a locally created object:
+Using color output via a locally created object:
 
 ```js
-var con = require('manakin').local; // returns a new object for local usage 
+var con = require('manakin').local; // returns a new local object 
 
-con.warn(val1, val2, ...);  // yellow output for the local warnings
-con.error(val1, val2, ...); // red output for the local errors
+con.log(val1, val2, ...);  // redirects to the default implementation
+con.warn(val1, val2, ...);  // uses yellow color
+con.error(val1, val2, ...); // uses red color
 ```
 
-Setting bright colors locally:
+Setting bright colors:
 
 ```js
 var con = require('manakin').local;
 
-con.warn.bright = true; // use bright yellow for the local warnings;
-con.error.bright = true; // use bright red for the local errors;
+con.log.bright = true; // use bright white for `con.log`;
+con.warn.bright = true; // use bright yellow for `con.warn`;
+con.error.bright = true; // use bright red for `con.error`;
 ```
 
 ### shared
@@ -53,8 +60,9 @@ Using color output with the module-wide object:
 ```js
 var con = require('manakin'); // returns the shared/root object 
 
-con.warn(val1, val2, ...);  // yellow output for the shared object
-con.error(val1, val2, ...); // red output for the shared object
+con.log(val1, val2, ...);  // redirects to the default implementation
+con.warn(val1, val2, ...);  // uses yellow color
+con.error(val1, val2, ...); // uses red color
 ```
 
 Setting bright colors for the module:
@@ -62,8 +70,9 @@ Setting bright colors for the module:
 ```js
 var con = require('manakin');
 
-con.warn.bright = true; // use bright yellow for the shared object;
-con.error.bright = true; // use bright red for the shared object;
+con.log.bright = true; // use bright white for `con.log`;
+con.warn.bright = true; // use bright yellow for `con.warn`;
+con.error.bright = true; // use bright red for `con.error`;
 ```
 
 [&lt;&lt; Main Page](https://github.com/vitaly-t/manakin)
--- test/mainSpec.js
+++ test/mainSpec.js
@@ -6,15 +6,15 @@ var capture = require('./capture');
 describe("protocol", function () {
 
     it("must expose complete protocol from the root", function () {
-        expect(Object.keys(lib)).toEqual(['error', 'warn', 'local', 'global']);
+        expect(Object.keys(lib)).toEqual(['log', 'error', 'warn', 'local', 'global']);
     });
 
     it("must expose main methods from local", function () {
-        expect(Object.keys(lib.local)).toEqual(['error', 'warn']);
+        expect(Object.keys(lib.local)).toEqual(['log', 'error', 'warn']);
     });
 
     it("must expose main methods from global", function () {
-        expect(Object.keys(lib.global)).toEqual(['error', 'warn']);
+        expect(Object.keys(lib.global)).toEqual(['log', 'error', 'warn']);
     });
 
 });
@@ -47,8 +47,10 @@ describe("formatting", function () {
         it("must match the regular console output", function () {
             inputs.forEach(function (values) {
                 var original = capture(console.log, values);
+                var log = capture(lib.log, values);
                 var warning = capture(lib.warn, values);
                 var error = capture(lib.error, values);
+                expect(log).toBe(original);
                 expect(warning).toBe(original);
                 expect(error).toBe(original);
             });
@@ -67,8 +69,10 @@ describe("formatting", function () {
 
         it("must match the regular console output", function () {
             var original = capture(console.log, ['hello']);
+            var log = capture(lib.log, ['hello']);
             var warning = capture(lib.warn, ['hello']);
             var error = capture(lib.error, ['hello']);
+            expect(log).toBe(original);
             expect(warning).toBe(original);
             expect(error).toBe(original);
         });
@@ -79,15 +83,19 @@ describe("formatting", function () {
 describe("bright colors", function () {
 
     var con1 = lib.local, con2 = lib.local, values = ['some\ntext'];
+    con1.log.bright = true;
     con1.warn.bright = true;
     con1.error.bright = true;
 
     it("must change the colors", function () {
-        var warn1 = capture.call(con1, con1.warn, values, true),
+        var log1 = capture.call(con1, con1.log, values, true),
+            log2 = capture.call(con2, con2.log, values, true),
+            warn1 = capture.call(con1, con1.warn, values, true),
             warn2 = capture.call(con2, con2.warn, values, true),
             error1 = capture.call(con1, con1.error, values, true),
             error2 = capture.call(con2, con2.error, values, true);
 
+        expect(log1 === log2).toBe(false);
         expect(warn1 === warn2).toBe(false);
         expect(error1 === error2).toBe(false);
     });
@@ -97,20 +105,25 @@ describe("global", function () {
 
     var glb = lib.global, loc = lib.local, values = ['some\ntext'];
 
+    loc.log.bright = true;
     loc.warn.bright = true;
     loc.error.bright = true;
 
     it("must change the colors", function () {
-        var warn1 = capture.call(loc, loc.warn, values, true),
+        var log1 = capture.call(loc, loc.log, values, true),
+            log2 = capture(glb.log, values, true),
+            warn1 = capture.call(loc, loc.warn, values, true),
             warn2 = capture(glb.warn, values, true),
             error1 = capture.call(loc, loc.error, values, true),
             error2 = capture(glb.error, values, true);
 
+        expect(log1 === log2).toBe(false);
         expect(warn1 === warn2).toBe(false);
         expect(error1 === error2).toBe(false);
     });
 
     // and this is just for coverage:
+    console.log();
     console.warn();
     console.error();
 });
